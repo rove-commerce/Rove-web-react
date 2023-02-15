197c401afa6cb6065076f1c0b217947379ce6013
Henrik Lundin <henke.lundin@gmail.com>
2023-02-15 22:41:12 +0100

    Footer nav

--- components/FooterNavigation.tsx
+++ components/FooterNavigation.tsx
@@ -1,25 +1,20 @@
 import Link from 'next/link'
-import Logo from './logo'
-import IconLinkedIn from './icon-linkedin'
+import Logo from './Logo'
+import IconLinkedIn from './IconLinkedin'
 
 export default function FooterNavigation() {
     return (
 <div className="bg-white py-12 border-t border-blue-light">
 		<div className="wrapper flex flex-col tablet-lg:flex-row justify-between items-center">
-			<Link href="/"><Logo className="text-blue w-12" symbols-color="#FFD5CF"></Logo></Link>
+			<Link href="/"><Logo className="text-blue w-12" symbolsColor="#FFD5CF"></Logo></Link>
 			<div className="flex mt-6 tablet-lg:mt-0 items-center">
 				<span className="text-blue text-12">Följ oss på</span>
-				<a href="https://www.linkedin.com/company/rove" rel="noopener" target="_blank">
-					<icon-linked-in className="text-blue w-10"></icon-linked-in>
+				<a href="https://www.linkedin.com/company/rove" rel="noreferrer" target="_blank">
+					<IconLinkedIn className="text-blue w-10"></IconLinkedIn>
 				</a>
-				<!-- Facebook
-                <a href="#" rel="noopener" class="ml-5">
-                    <icon-facebook className="text-blue w-10"></icon-facebook>
-                </a>
-                -->
 			</div>
 			<div className="text-blue text-12 mt-6 tablet-lg:mt-0">
-				<a href="https://www.litium.se" rel="noopener" target="_blank">
+				<a href="https://www.litium.se" rel="noreferrer" target="_blank">
 					<img className="w-24" alt="Litium Partner" src="/Litium2020-Logo-RGB_Black.png" />
 				</a>
 			</div>
@@ -30,7 +25,7 @@ export default function FooterNavigation() {
 				<Link href="/privacy/" className="ml-4 hover:underline">Cookies & Integritetspolicy</Link>
 			</div>
 			<div className="text-blue text-12 mt-6 tablet-lg:mt-0">
-				<span>© 2022 Rove Commerce Advisory AB</span>
+				<span>© 2023 Rove Commerce Advisory AB</span>
 			</div>
 		</div>
 	</div>
