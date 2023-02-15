e8d9ea097a51a6acb7b328a31d28a063a55ee76c
Henrik Lundin <henke.lundin@gmail.com>
2023-02-15 16:39:17 +0100

    Tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world!!!
     </h1>
   )
 }
