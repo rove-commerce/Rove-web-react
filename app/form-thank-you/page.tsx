3450c0164339260c25234a345bef0be1bf352b4f
Henrik Lundin <henke.lundin@gmail.com>
2023-02-20 09:52:53 +0100

    Form thank you page

---
+++ app/form-thank-you/page.tsx
@@ -0,0 +1,14 @@
+import PageHeader from "../../components/PageHeader";
+
+export default function Home() {
+  return (
+    <>
+      <PageHeader>Tack!</PageHeader>
+      <div className="wrapper">
+        <div className="inner-wrapper mt-8">
+          <p>Tack för ditt svar, vi tittar på det så snart vi har möjlighet.</p><br />
+        </div>
+      </div>
+    </>
+  );
+}
