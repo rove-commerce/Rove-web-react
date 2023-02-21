6d22fa2eec3e3d683687c60ca56c7b89503f1ae6
Henrik Lundin <henke.lundin@gmail.com>
2023-02-21 13:58:57 +0100

    Privacy

---
+++ app/privacy/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Cookies och Integritetspolicy | Rove</title>
+      <meta name="description" content="Privacy, Cookies och Integritetspolicy"/>
+    </>
+  )
+}
