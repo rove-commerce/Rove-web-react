e230b60576e6fa7d0590b25edd6a767000cb3b41
Henrik Lundin <henke.lundin@gmail.com>
2023-02-21 11:51:32 +0100

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import "./globals.css";
 import MainNavigation from "../components/MainNavigation";
 import FooterNavigation from "../components/FooterNavigation";
+import CookieConsent from "../components/CookieConsent";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const GA_MEASUREMENT_ID = "G-GW944XXWFN";
   return (
     <html lang="sv">
       {/*
@@ -14,6 +17,18 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <body>
         <MainNavigation></MainNavigation>
         {children}
