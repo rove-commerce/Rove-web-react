64188506df8ff99f804ea59c7b16fe9ca973df2e
Henrik Lundin <henke.lundin@gmail.com>
2023-02-23 14:06:10 +0100

    Test reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
+import BlogSummary from "../components/BlogSummary";
+import ContactForm from "../components/ContactForm";
+import Jumbotron from "../components/Jumbotron";
+
 export default function Home() {
   return (
     <>
+      <Jumbotron />
+      <BlogSummary />
+      <ContactForm name="startpage-form" />
     </>
   );
 }
