4040476123bc183cb4657b729326abfd2f734ddc
Henrik Lundin <henke.lundin@gmail.com>
2023-02-23 14:03:09 +0100

    Test 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
-import BlogSummary from "../components/BlogSummary";
-import ContactForm from "../components/ContactForm";
-import Jumbotron from "../components/Jumbotron";
-
 export default function Home() {
   return (
     <>
-      <Jumbotron />
-      <BlogSummary/>
-      <ContactForm name="startpage-form" />
     </>
   );
 }
