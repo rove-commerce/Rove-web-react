6cdaa548aecd6256898b947a9a9f690be4c16fc4
Henrik Lundin <henke.lundin@gmail.com>
2023-02-23 14:37:58 +0100

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,18 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        async
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
 
       <body>
         <MainNavigation></MainNavigation>
