327d61a51f76aaafbff50f26b5a839793b5b09e3
Henrik Lundin <henke.lundin@gmail.com>
2023-02-23 14:44:49 +0100

    Ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
       */}
       <head />
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        src={`<script async src="https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         async
       />
       <Script id="google-analytics">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());         
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
