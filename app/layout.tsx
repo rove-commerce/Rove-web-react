6c00f10448ed10920bd6ec0fc755233240b63b78
Henrik Lundin <henke.lundin@gmail.com>
2023-03-06 10:46:50 +0100

    Disbale cookie bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,45 +3,51 @@ import MainNavigation from "../components/MainNavigation";
 import FooterNavigation from "../components/FooterNavigation";
 import CookieBar from "../components/CookieBar";
 import Script from "next/script";
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: {
-    default: "Rove | E-handel, MarTech och digital marknadsföring",
-    template: '%s | rove.se | Sverige',
-  },
-  keywords: ["Upphandling", "Projektledning", "Rådgivning", "Förstudie", "Digital handel", "E-handel", "PIM", "Digital marknadsföring", "Kundresan", "Köpresan", "Litium", "HubSpot"],
-  icons: {
-    icon: '/favicon.ico',
-  },
+	title: {
+		default: "Rove | E-handel, MarTech och digital marknadsföring",
+		template: "%s | rove.se | Sverige",
+	},
+	keywords: [
+		"Upphandling",
+		"Projektledning",
+		"Rådgivning",
+		"Förstudie",
+		"Digital handel",
+		"E-handel",
+		"PIM",
+		"Digital marknadsföring",
+		"Kundresan",
+		"Köpresan",
+		"Litium",
+		"HubSpot",
+	],
+	icons: {
+		icon: "/favicon.ico",
+	},
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const GA_MEASUREMENT_ID = "G-GW944XXWFN";
-  return (
-    <html lang="sv">
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	const GA_MEASUREMENT_ID = "G-GW944XXWFN";
+	return (
+		<html lang="sv">
+			<Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_MEASUREMENT_ID}');
         `}
-      </Script>
-      <body>
-        <MainNavigation />
-        {children}
-        <FooterNavigation />
-        <CookieBar />
-      </body>
-    </html>
-  );
+			</Script>
+			<body>
+				<MainNavigation />
+				{children}
+				<FooterNavigation />
+				{/*<CookieBar />*/}
+			</body>
+		</html>
+	);
 }
