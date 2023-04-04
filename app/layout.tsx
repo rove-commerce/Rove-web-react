79ac24110ba369897904d38fb88cbfebec0ce50a
Henrik Lundin <henke.lundin@gmail.com>
2023-04-04 10:06:41 +0200

    GTM fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="sv">
 			{/* Google Tag Manager */}
-			<Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+			<Script id="GTM" strategy="afterInteractive" dangerouslySetInnerHTML={{
 				__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
