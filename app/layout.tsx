cea44019ed4810430ac5f6db61561da772ec9e22
Henrik Lundin <henke.lundin@gmail.com>
2023-04-04 10:02:34 +0200

    Google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,18 +33,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	const GA_MEASUREMENT_ID = "G-GW944XXWFN";
 	return (
 		<html lang="sv">
+			{/* Google Tag Manager */}
+			<Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+				__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJMDBVM');`}}></Script>
+
+			{/*}
 			<Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
 			<Script id="google-analytics" strategy="afterInteractive">
 				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}');
-        `}
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){window.dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', '${GA_MEASUREMENT_ID}');
+				`}
 			</Script>
-			{/* Start of HubSpot Tracking Code */}
-			<Script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/8359383.js"></Script>
+			*/}
 			<body>
+				{/* Google Tag Manager (noscript) */}
+				<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJMDBVM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
 				<MainNavigation />
 				{children}
 				<FooterNavigation />
