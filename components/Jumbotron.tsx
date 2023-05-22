00b4c57a6357cf34c9d5ee02486101587645bc02
Henrik Lundin <henke.lundin@gmail.com>
2023-05-22 14:58:10 +0200

    Leadgen -> lead generation

--- components/Jumbotron.tsx
+++ components/Jumbotron.tsx
@@ -7,7 +7,7 @@ export default function Jumbotron() {
 				<div className="inner-wrapper">
 					<h1 className="text-white text-32 tablet:text-36 max-w-5xl leading-snug">
 						<span className="font-bold border-b-2 border-red">Rove</span> hjälper företag att bli framgångsrika i sin digitala affär genom e-handel
-						och leadgen
+						och lead generation
 					</h1>
 					<div className="inline-flex mt-8 flex-col tablet:flex-row">
 						<Link href="/tjanster" className="btn-white inline-block">
