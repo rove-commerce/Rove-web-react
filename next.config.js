8084209e95af27e8fbffde7b7fc80c3ce4689f43
Henrik Lundin <henke.lundin@gmail.com>
2023-05-24 14:22:15 +0200

    Removed exerimental app setting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    mdxRs: true,
-  },
-}
+	experimental: {
+		mdxRs: true,
+	},
+};
 
-const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+const withMDX = require("@next/mdx")();
+module.exports = withMDX(nextConfig);
